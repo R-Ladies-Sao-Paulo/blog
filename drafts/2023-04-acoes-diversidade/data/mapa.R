903515966d65cabcd7f74c7a960a39942ee479cb
Beatriz Milz <milz.bea@gmail.com>
2023-05-07 10:45:22 -0300

    adiciona conteúdo na pasta drafts

--- drafts/2023-04-acoes-diversidade/data/mapa.R
+++ drafts/2023-04-acoes-diversidade/data/mapa.R
@@ -1,5 +1,5 @@
 library(ggplot2)
-censo_2020 <- readr::read_rds("posts/2023-03-acoes-diversidade/data/censo_2020.rds")
+censo_2020 <- readr::read_rds("drafts/2023-04-acoes-diversidade/data/censo_2020.rds")
 
 sumarizacao_onde_mora <- censo_2020 |> 
   dplyr::count(onde_mora)
@@ -18,17 +18,18 @@ sumarizacao_onde_mora <- censo_2020 |>
 # Região metropolitana de SP
 # http://datageo.ambiente.sp.gov.br/geoportal/catalog/search/resource/details.page?uuid=%7B029C065B-F460-4039-AD90-09B50AC6E5A0%7D
 
-shape_rmsp <- "~/Downloads/REGIOES_METROPOLITANAS_2016_POL/REGIOES_METROPOLITANAS_2016_POL.shp"
+shape_rmsp <- "drafts/2023-04-acoes-diversidade/data/REGIOES_METROPOLITANAS_2016_POL/REGIOES_METROPOLITANAS_2016_POL.shp"
 rmsp <- sf::read_sf(shape_rmsp) |> 
   dplyr::filter(Nome == "Regiao Metropolitana de Sao Paulo") |> 
   tibble::add_column(n_respostas = c(21)) |> 
   dplyr::mutate(centroide = sf::st_centroid(geometry),
                 long = purrr::map_dbl(centroide, ~.x[1]),
-                lat = purrr::map_dbl(centroide, ~.x[2]))
+                lat = purrr::map_dbl(centroide, ~.x[2])) |> 
+  dplyr::mutate(Nome_en = "Metropolitan Region of Sao Paulo")
 
 
 # https://datageo.ambiente.sp.gov.br/app/?ctx=DATAGEO#
-distritos_sp <- "~/Downloads/DISTRITO_MUNICIPAL_SP_SMDU/DISTRITO_MUNICIPAL_SP_SMDUPolygon.shp"
+distritos_sp <- "drafts/2023-04-acoes-diversidade/data/DISTRITO_MUNICIPAL_SP_SMDU/DISTRITO_MUNICIPAL_SP_SMDUPolygon.shp"
 shp_distritos_sp <- sf::read_sf(distritos_sp)
 
 # https://www.prefeitura.sp.gov.br/cidade/secretarias/upload/subprefeituras/Fotos%20Site/Teste_mapa_prefeituras.jpg
@@ -64,7 +65,17 @@ shp_regioes_sp_resp <-  shp_regioes_sp |>
   dplyr::mutate(centroide = sf::st_centroid(geometry),
                 long = purrr::map_dbl(centroide, ~.x[1]),
                 lat = purrr::map_dbl(centroide, ~.x[2])) |> 
-  dplyr::mutate(cor = dplyr::if_else(n_respostas > 30,"white", "black"))
+  dplyr::mutate(cor = dplyr::if_else(n_respostas > 30,"white", "black")) |> 
+  dplyr::mutate(
+    regiao_en = dplyr::case_match(
+      regiao,
+      "Centro" ~ "",
+      "Leste" ~ "East",
+      "Norte" ~ "North",
+      "Oeste" ~ "West",
+      "Sul" ~ "South"
+    )
+  )
  
 
 # mapa
@@ -81,4 +92,22 @@ ggplot() +
   theme_void() +
   theme(legend.position = "top") +
   labs(fill = "Quantidade de respostas") 
+
+
+# em INGLES
+
+ggplot() +
+  geom_sf(aes(fill = n_respostas),  data = rmsp) +
+  geom_sf(aes(fill = n_respostas), data = shp_regioes_sp_resp) +
+  geom_text(aes(x = long, y = lat, label = regiao_en, color = cor), size = 3, 
+            data = shp_regioes_sp_resp) + 
+  geom_text(aes(x = long *0.992, y = lat, label = Nome_en), size = 2.5, 
+            color = "black",
+            data = rmsp) + 
+  scale_fill_distiller(direction = 1, palette = "Purples") + 
+  scale_color_identity() + 
+  theme_void() +
+  theme(legend.position = "bottom", 
+        plot.title = element_text(hjust = 0.5, color = "#562357")) +
+  labs(fill = "", title = "Number of responses in the City of Sao Paulo \n and the Metropolitan Region of Sao Paulo \n") 
 
